2dda9397fbe7b08354c886a7e3c26ef4ac67f1bf
SzymDan <sn.danielak@gmail.com>
2021-11-16 11:26:16 -0500

    SSO

--- crm1_v14_registration_login/crm1/urls.py
+++ crm1_v14_registration_login/crm1/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls'))
+    path('', include('accounts.urls')),
+    path('oauth/', include('social_django.urls', namespace='social')),
 
 ]
+
+
