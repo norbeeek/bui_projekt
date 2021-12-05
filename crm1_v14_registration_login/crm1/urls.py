0a194dc86498e26405cb68315e4ab36dbb0dd83f
MichalR13 <82214165+MichalR13@users.noreply.github.com>
2021-12-05 12:28:07 +0100

    admin off
    admin off

--- crm1_v14_registration_login/crm1/urls.py
+++ crm1_v14_registration_login/crm1/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
 
